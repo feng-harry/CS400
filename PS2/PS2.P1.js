9f84032c68a2e7df26dfe19280015f6486747f97
Harry Feng <hfeng125@bu.edu>
2019-09-26 00:17:29 -0400

    finished with P1

---
+++ PS2/PS2.P1.js
@@ -0,0 +1,44 @@
+const sum_array = input_array => {
+    const sum_values = (accumulator, currentValue) => {
+        return accumulator + currentValue;
+    }
+    return input_array.reduce(sum_values);
+}
+
+const swap_and_update = (input_array, value) => {
+    input_array[0] = input_array[1]
+    input_array[1] = value;
+}
+
+function* fib1() {
+    let seq_3 = [0,1];
+    let i = 0;
+    while (true) {
+        if (i < 2) {
+            i++;
+            yield seq_3[i-1];
+        }
+        else{
+            let result = sum_array(seq_3);
+            swap_and_update(seq_3, result);
+            yield result;
+        }
+    }
+}
+
+function* fib2() {
+    let fib = fib1();
+    while (true) {
+        let result = fib.next().value;
+        while (result % 2 == 1) {
+            result = fib.next().value;
+        }
+        yield result;
+    }
+}
+
+let x = fib2();
+
+for (let i = 0; i < 6; i++) {
+    console.log(x.next().value);
+}
