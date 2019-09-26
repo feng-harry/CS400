7d583921d8422fbde6eeae3c8ce72b35f5360e50
Harry Feng <hfeng125@bu.edu>
2019-09-26 15:57:12 -0400

    Finished with P2

---
+++ PS2/PS2.P2.js
@@ -0,0 +1,21 @@
+function* print_words(sentence) {
+    let string_as_array = sentence.split(" ");
+    let i = 0;
+    let finished = false;
+    while (!finished){
+        let res = string_as_array[i];
+        i++;
+        if (i >= string_as_array.length) {
+            finished = true;
+        }
+        yield res;
+    }
+}
+
+const input_str = "All I know is something like a bird within her sang"
+let x = print_words(input_str);
+for (let i = 0; i < input_str.split(" ").length; i++) {
+    console.log(x.next().value);
+}
+
+
